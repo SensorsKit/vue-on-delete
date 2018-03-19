2b4d3e142b250f44703a4981fdb060c347166730
TianGong <397009765@qq.com>
2018-03-19 15:29:31 +0800

    fix(core): rewrite isDelete.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,41 @@ const log = {
   }
 }
 
-OnDeletePlugin.install = isDelete
+OnDeletePlugin.install = function(Vue, options) {
+  Vue.directive('on-delete', {
+    bind(el, binding, vnode) {
+      const onDelete = binding.value
+      if (typeof onDelete !== 'function') {
+        log.e('指令需要传入一个函数！')
+        return
+      }
+
+      // 逻辑...
+      let [strOld, strNew] = [null, null]
+      const onKeyDown = () => {
+        strOld = el.value
+      }
+
+      const onKeyUp = () => {
+        strNew = el.value
+        if (isDelete(strOld, strNew)) {
+          binding.value()
+        }
+      }
+      el.addEventListener('keydown', onKeyDown, false)
+      el.addEventListener('keyup', onKeyUp, false)
+    },
+
+    update(el, binding, vnode, oldVnode) {
+
+    },
+
+    unbind(el, binding, vnode) {
+      // 解除事件监听
+      el.removeEventListener('keydown', onKeyDown, false)
+      el.removeEventListener('keyup', onKeyUp, false)
+    }
+  })
+}
 
 export default OnDeletePlugin
