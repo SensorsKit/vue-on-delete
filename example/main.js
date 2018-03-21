174bf086b3f65eced60ff7f03dffd0114caf4760
newraina <newraina@users.noreply.github.com>
2018-03-21 16:35:01 +0800

    add vconsole for dev debug

--- example/main.js
+++ example/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App'
-import VueOnDelete from '../src'
+import VueOnDelete from '../src/index'
+
+const hostname = window.location.hostname
+
+if (
+  hostname === 'localhost' ||
+  !Number.isNaN(Number(hostname.split('.').join('')))
+) {
+  import('vconsole').then(result => new result.default())
+}
 
 Vue.use(VueOnDelete)
 
